7489219fe3ae50ead11fc90a93031c1f91643ec8
Chris Pietschmann <chris@build5nines.com>
2026-06-07 15:52:40 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@ dotnet test
 
 dotnet build
 
-./src/Powergentic.Flow.Cli/bin/Debug/net10.0/pgflow run samples/script-and-copilot-loop workspace --var userPrompt="Build initial scaffolding for  TODO app for a small team"
+./src/Powergentic.Flow.Cli/bin/Debug/net10.0/pgflow run samples/script-and-copilot-loop workspace --display-enhanced --var userPrompt="Build initial scaffolding for  TODO app for a small team"
+
+# ./src/Powergentic.Flow.Cli/bin/Debug/net10.0/pgflow logs samples/script-and-copilot-loop --latest
